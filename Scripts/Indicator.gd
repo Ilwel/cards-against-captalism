afdfe7ac92d4da4d199f6af8d9c944606b26fecd
iueu <ilwelisaac@hotmail.com>
2026-01-08 20:01:40 -0300

    feat: indicators name ajustments

--- Scripts/Indicator.gd
+++ Scripts/Indicator.gd
@@ -17,10 +17,18 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if animation == "hearts":
 		value_lbl.text  = str(Match.get_alienation())
+		indicator_name_lbl.modulate = Color("#f38ba8")
+		value_lbl.modulate = Color("#f38ba8")
 	elif animation == "spades":
 		value_lbl.text  = str(Match.get_exploitation())
+		indicator_name_lbl.modulate = Color("#cdd6f4")
+		value_lbl.modulate = Color("#cdd6f4")
 	elif animation == "diamonds":
 		value_lbl.text  = str(Match.get_capital())
+		indicator_name_lbl.modulate = Color("#f9e2af")
+		value_lbl.modulate = Color("#f9e2af")
 	elif animation == "clubs":
 		value_lbl.text  = str(Match.get_bureaucracy())
+		indicator_name_lbl.modulate = Color("8bafafff")
+		value_lbl.modulate = Color("8bafafff")
 		
